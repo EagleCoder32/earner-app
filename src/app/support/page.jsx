af30ec431992f452eadd6f4cc38e97ab3f25f6ea
Hind Eagle <earnereagle@gmail.com>
2025-07-06 18:47:51 +0530

    support

---
+++ src/app/support/page.jsx
@@ -0,0 +1,17 @@
+// src/app/support/page.jsx
+'use client';
+
+export default function SupportPage() {
+    return (
+        <div>
+            <div>
+                {/* Page Title */}
+                <h1 className="text-white text-2xl font-bold p-6">Send Message</h1>
+
+                {/* Embedded Google Form */}
+                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdJwjnZ9qMJRPNn8efZDnghWRaGSjfqhbzZCkD7y2Tg968y-Q/viewform?embedded=true" width="640" height="687" marginheight="0" marginwidth="0">Loading…</iframe>
+
+            </div>
+        </div>
+    );
+}
